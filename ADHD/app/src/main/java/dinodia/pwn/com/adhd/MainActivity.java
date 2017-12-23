7040184d1b157ca3deb332e9a5a7db286e926e7a
Pawan Dinodia <pawandinodiabme@gmail.com>
2017-12-23 23:01:30 +0530

    Wake lock added

--- ADHD/app/src/main/java/dinodia/pwn/com/adhd/MainActivity.java
+++ ADHD/app/src/main/java/dinodia/pwn/com/adhd/MainActivity.java
@@ -1,6 +1,9 @@
 package dinodia.pwn.com.adhd;
 
 import java.util.Calendar;
+
+import android.content.Context;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,11 +20,18 @@ public class MainActivity extends AppCompatActivity {
     int last_hour=0;
 
     int max_sec=0;
+
+    protected PowerManager.WakeLock mWakeLock;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        this.mWakeLock = pm.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "LOCK");
+        this.mWakeLock.acquire();
+
         final TextView counter_tv = (TextView) findViewById(R.id.counterTv);
         Button addCount = (Button) findViewById(R.id.addBtn);
 
@@ -42,9 +52,9 @@ public class MainActivity extends AppCompatActivity {
 
                 if(max_sec<total_new_sec){
                     max_sec=total_new_sec;
-                    max_time.setText(("Max:-").concat(convertTime(max_sec)));
+                    max_time.setText(("Best time:\n").concat(convertTime(max_sec)));
                 }
-                last_time.setText(("Last:-").concat(convertTime(total_new_sec)));
+                last_time.setText(("Last time:\n").concat(convertTime(total_new_sec)));
 
                 last_hour = Calendar.getInstance().get(Calendar.HOUR);
                 last_min = Calendar.getInstance().get(Calendar.MINUTE);
